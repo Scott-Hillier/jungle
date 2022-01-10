4e7a1275160dcb9802dc4a70d9fa14cddb908991
Scott Hillier <scotthhillier@gmail.com>
2022-01-10 15:48:10 -0800

    first test runs properly

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    it 'ensures that a product will save properly' do
+      category = Category.create(name: "Apparel")
+      product = Product.create(name: "this", price: 234333, quantity: 2, category: category)
+      expect(product).to be_present
+    end
+  end
+end

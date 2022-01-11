11456d1785299f7d1e269a74a75430f3917f81b6
Scott Hillier <scotthhillier@gmail.com>
2022-01-10 17:02:39 -0800

    merging master to undo some changes

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,5 +7,23 @@ RSpec.describe Product, type: :model do
       product = Product.create(name: "this", price: 234333, quantity: 2, category: category)
       expect(product).to be_present
     end
+
+    it 'ensures that a product will save properly' do
+      category = Category.create(name: "Apparel")
+      product = Product.create(name: "this", price: 234333, quantity: 2, category: category)
+      expect(product).to be_present
+    end
+
+    it 'ensures that a product will save properly' do
+      category = Category.create(name: "Apparel")
+      product = Product.create(name: "this", price: 234333, quantity: 2, category: category)
+      expect(product).to be_present
+    end
+
+    it 'ensures that a product will save properly' do
+      category = Category.create(name: "Apparel")
+      product = Product.create(name: "this", price: 234333, quantity: 2, category: category)
+      expect(product).to be_present
+    end
   end
 end
